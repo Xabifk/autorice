ea1b3d17ae89e209f4fa7fcaffea7a2d50e35350
Xabifk <adelintataru87@gmail.com>
2020-06-28 23:16:39 +0300

    Fix order in install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,9 @@
 
 echo -n "Please input a username: "
 read username
-
-scripts/install_dependencies.sh
-
 scripts/user_setup.sh $username
 
+scripts/install_dependencies.sh $username
 
 # the user needs to be able to use sudo without inputing the passwords so it doesn't ask constantly
 sed -i "s/# %sudo ALL=(ALL) ALL/%sudo ALL=(ALL) NOPASSWD: ALL/g" /etc/sudoers
