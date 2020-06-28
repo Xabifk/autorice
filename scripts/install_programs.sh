e34686d6420979be71ff5b47dc40704cbcaf45da
Xabifk <adelintataru87@gmail.com>
2020-06-28 22:22:41 +0300

    Flushed out install_programs.sh

--- scripts/install_programs.sh
+++ scripts/install_programs.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
 	echo "Usage: $0 <username>"
 	exit 1
 fi
+
+echo "[ Installing programs ]"
+
+while IFS=, read -r place name
+do
+	place=$(echo $place | xargs echo -n)
+	name=$(echo $name | xargs echo -n)
+
+    	echo "I got:$place|$name"
+	if [ $place = "repo" ]
+	then
+		echo "[ Installing $name from Repository ]"
+		pacman --noconfirm --needed -S $name >/dev/null 2>&1 || echo "[ Could not install $name form Repository ]"; exit 1
+	fi
+
+	if [ $place = "aur" ]
+	then
+		echo "[ Installing $name from AUR ]"
+		sudo -u "$1" yay -S --noconfirm "$name" >/dev/null 2>&1 || echo "[ Could not install $name from AUR ]"; exit 1
+	fi
+
+	if [ $place = "script" ]
+	then
+		echo "[ Running script custom_scripts/$name ]"
+		./custom_scripts/$name >/dev/null 2>&1 || echo "[ Could not run script custom_scripts/$name ]"
+	fi
+done < configs/programs.csv
+
+echo "[ Finished installing programs ]"
