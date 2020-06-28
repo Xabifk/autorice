0fd12b52cbf6159a05e0aa19e3746ae4457c1b27
Xabifk <adelintataru87@gmail.com>
2020-06-28 22:47:51 +0300

    Add sudo nopassword

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ scripts/install_dependencies.sh
 
 scripts/user_setup.sh
 
+
+# the user needs to be able to use sudo without inputing the passwords so it doesn't ask constantly
+sed -i "s/# %sudo ALL=(ALL) ALL/%sudo ALL=(ALL) NOPASSWD: ALL/g" /etc/sudoers
+
 scripts/install_drivers.sh $username
 
 scripts/install_programs.sh $username
