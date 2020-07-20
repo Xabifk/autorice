be97e45fbd76df328df4593f53d4659176869d7e
Xabifk <adelintataru87@gmail.com>
2020-07-20 09:55:55 +0300

    Fix switch to workspace 7+

--- custom_scripts/set_gnome_shortcuts.sh
+++ custom_scripts/set_gnome_shortcuts.sh
@@ -3,8 +3,14 @@
 # workspace keybinds
 for i in {1..10}
 do
-	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i ["'<Super>$i'"]
-	gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i ["'<Super><Shift>$i'"]
+	gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i ["'<Super>$(($i % 10))'"]
+	gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i ["'<Super><Shift>$(($i % 10))'"]
+done
+
+for i in {1..9}
+do
+
+	gsettings set org.gnome.shell.keybindings switch-to-application-$i ["''"]
 done
 
 # window positioning
